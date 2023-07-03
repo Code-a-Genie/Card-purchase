fe234b27b75f65bf9e946561c19b52a5df15ad07
CodeAGenie <nwibeg@gmail.com>
2023-07-03 02:33:11 +0100

    js update

---
+++ card.js
@@ -0,0 +1,10 @@
+const confirm = document.getElementById("confirm")
+confirm.addEventListener('submit' handleSubmit)
+const userName = 
+
+
+
+function handleSubmit(e){
+    e.preventDefault();
+
+}
