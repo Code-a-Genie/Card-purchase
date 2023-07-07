f643af13cd57775fab519ee8671072c4ad88b144
CodeAGenie <nwibeg@gmail.com>
2023-07-07 14:21:06 +0100

    update-figuring it out

--- card.js
+++ card.js
@@ -1,10 +1,38 @@
-const confirm = document.getElementById("confirm")
-confirm.addEventListener('submit' handleSubmit)
-const userName = 
+const submitCard = document.getElementById("confirm")
+submitCard.addEventListener('submit', execute)
+
+const userName = document.getElementById("cardHolder")
+userName.addEventListener('input',checkname)
+const userCardNumber= document.getElementById("cardNumber")
+const cardMonth = document.getElementById("month")
+const cardYear=document.getElementById("year")
+const cardCode = document.getElementById("cvc")
+let cardDisplay =document.getElementById("cardUpdate").addEventListener('Input', display)
+cardDisplay.innerText= currentCardName;
+let currentCardName = userName.value;
+let currentCardNumber = userCardNumber.value;
+let currentMonth = cardMonth.value;
+let currentYear = cardYear.value;
+let currentCvc = cardCode.value;
+let errorMessage =document.getElementById("errorText")
 
 
 
-function handleSubmit(e){
-    e.preventDefault();
+// function execute(e){
+//     e.preventDefault();
+//     if(currentCardName===" "){
+//         return errorMessage;
+//     }else{
+//         return currentCardName;
+      
+//     } 
+// }
 
+function checkname(){
+    if(currentCardName===" "){
+                return errorMessage;
+            }
+            else{
+                return currentCardName;
+}
 }
